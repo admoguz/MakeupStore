4c912b4db2ef9933b64666a6a8f1f7e4deab783b
Oguz Ayric <oguzayrc@gmail.com>
2022-06-17 20:11:42 +0300

    home view model service

---
+++ src/Web/Models/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PriceTry => Price.ToString("c2");
+
+        public string PictureUri { get; set; }      
+    }
+}
